e47cf7be9814a890e71d53a9878a75c672551037
mohammad hossein <mohammad.hajiebrahimi83@gmail.com>
2023-12-16 21:49:09 +0330

    add input func

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
 
+#include <bits/stdc++.h>
 
 #include "src/bmp.cpp"
-
-
+using namespace std;
+#define sep ' '
+#define endl '\n'
+#define tab '\t'
+const string BLUR = "-B";
+const string SHARPEN = "-S";
+const string EMBOSS = "-E";
+const string INVERT = "-I";
+const string GRAYSCALE = "-G";
+const string DELIMITER = ":";
 
 
 /*
@@ -24,12 +32,42 @@ The (0, 0) pixel is the top-left corner of the image.
 So we have data[row][column]
 The bottom-right pixel is: bmp.data[bmp.infoHdr.height - 1][bmp.infoHdr.width - 1]
 */
-
-int main() {
-    Bmp bmp;
-    create(bmp, 100, 200);
-    bmp.data[0][0] = Pixel(255, 0, 0);
-    write(bmp, "test.bmp");
-    delete bmp.fileData;
+void do_filters(int argc, char **argv){
+    for (int i=0;i<argc;i++){
+        string input;
+        if (strcmp(argv[i], "-B") == 0){
+            input = BLUR;
+        }
+        else if (strcmp(argv[i], "-S") == 0){
+            input = SHARPEN;
+        }
+        else if (strcmp(argv[i], "-E") == 0){
+            input = EMBOSS;
+        }
+        else if (strcmp(argv[i], "-I") == 0){
+            input = INVERT;
+        }
+        else if (strcmp(argv[i], "-G") == 0){
+            input = GRAYSCALE;
+        }
+        if (i!=argc-1){
+            string str(argv[i+1]);
+            if(str.find(DELIMITER)<str.length()){
+                //go and filter
+                cout<<input<<sep<<str;
+            }
+            else{cout<<input<<endl;}
+        }
+        else{
+            cout<<input<<endl;
+        }
+    }
+}
+int main(int argc, char **argv) {
+    string inputfile,outputfile;
+    while(cin>>inputfile>>outputfile){
+        cout<<inputfile<<sep<<outputfile;
+        do_filters(argc,argv);
+    }
     return 0;
 }
